74a2e6a84ef7edfd9cfd69f18b22d0fa6dcf7893
Mark Danielle Bien <mark.bien@outlook.com>
2022-05-06 03:39:53 +0800

    Complete the Home page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,84 @@
-alert("test");
+import './style.css';
+
+const $ = document.querySelector.bind(document);
+const $create = document.createElement.bind(document);
+const body = $('body');
+body.classList.add('home');
+
+const content = $create('div');
+content.setAttribute('id', 'content');
+body.appendChild(content);
+
+function navComponent(){
+    const nav = $create('nav');
+    const ul = $create('ul');
+    const tabs = ['Home', 'Menu', 'Contact Us'];
+    
+    nav.appendChild(ul);
+    tabs.forEach(tab => {
+        const li = $create('li');
+        li.textContent = tab;
+        ul.appendChild(li);
+    });
+
+    return nav;
+}
+
+function footerComponent(){
+    const footer = $create('footer');
+    footer.textContent = "Baktol Restaurant Inc.";
+
+    return footer;
+}
+
+function homeTabComponent(){
+    const homeTab = $create('div');
+    homeTab.classList.add('home-tab');
+
+    const top = $create('div');
+    top.classList.add('top');
+
+    const companyName = $create('h1');
+    companyName.textContent = "Baktol Restaurant";
+
+    const hr = $create('hr');
+
+    const history = $create('p');
+    history.textContent = "Founded in 2022 by Parsley and Lexi. It started as a small coffee shop that is pet-friendly and extended its services to provide top tier delicacies and dishes.";
+    history.classList.add('text-justify');
+
+    const bottom = $create('div');
+    bottom.classList.add('bottom');
+
+    const vision = $create('h3');
+    vision.textContent = "Vision";
+
+    const visionText = $create("p");
+    visionText.classList.add('text-justify');
+    visionText.textContent = "A leading 5 star restaurant that has served millions of customers with delicious and extravagant dishes. Known to many as Miracle-Resto.";
+
+    const mission = $create('h3');
+    mission.textContent = "Mission";
+
+    const missionText = $create("p");
+    missionText.classList.add('text-justify');
+    missionText.textContent = "To provide world class service and dishes at a low price to ensure everyone gets to enjoy our foods and services.";
+
+    top.appendChild(companyName);
+    top.appendChild(hr);
+    top.appendChild(history);
+
+    bottom.appendChild(vision);
+    bottom.appendChild(visionText);
+    bottom.appendChild(mission);
+    bottom.appendChild(missionText);
+
+    homeTab.appendChild(top);
+    homeTab.appendChild(bottom);
+
+    return homeTab;
+}
+
+body.insertBefore(navComponent(), content);
+body.appendChild(footerComponent());
+content.appendChild(homeTabComponent());
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -7,10 +8,23 @@ module.exports = {
         new HtmlWebpackPlugin({
             title: "Restaurant Page",
         }),
+        new MiniCssExtractPlugin(),
     ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
-    },
+    },    
 };
