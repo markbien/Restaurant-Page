8f3f70bda7ad231e356779008ced69fa92e3da86
Mark Bien <48498353+markbien@users.noreply.github.com>
2024-10-04 13:46:48 +0800

    Resolve error: 'Cannot get /'

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+    })
+  ],
+};
