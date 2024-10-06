9ad8b5e56b90b09483dbed244d09624023f2d234
Mark Bien <48498353+markbien@users.noreply.github.com>
2024-10-06 18:19:07 +0800

    Modify createLandingPage function to accept argument

--- src/modules/landing-page.js
+++ src/modules/landing-page.js
@@ -1,27 +1,27 @@
-import fbIcon from '../img/facebook.png';
-import igIcon from '../img/instagram.png';
-import tiktokIcon from '../img/tiktok.png';
+import fbIcon from "../img/facebook.png";
+import igIcon from "../img/instagram.png";
+import tiktokIcon from "../img/tiktok.png";
 
-function createHeader(){
-  const header = document.createElement('header');
+function createHeader() {
+  const header = document.createElement("header");
 
-  const divLogo = document.createElement('div');
+  const divLogo = document.createElement("div");
   divLogo.setAttribute("id", "logo");
 
-  const restaurantName = document.createElement('h1');
+  const restaurantName = document.createElement("h1");
   restaurantName.textContent = "LMP Restaurant";
 
-  const motto = document.createElement('p');
+  const motto = document.createElement("p");
   motto.textContent = "The best pet-friendly restaurant in town!";
 
   divLogo.appendChild(restaurantName);
   divLogo.appendChild(motto);
   header.appendChild(divLogo);
 
-  const nav = document.createElement('nav');
+  const nav = document.createElement("nav");
   const options = ["Home", "Menu", "Contact Us"];
-  options.forEach(option => {
-    const btn = document.createElement('button');
+  options.forEach((option) => {
+    const btn = document.createElement("button");
     btn.textContent = option;
 
     nav.appendChild(btn);
@@ -32,31 +32,30 @@ function createHeader(){
   return header;
 }
 
-function createContentBody(){
-  const contentBody = document.createElement('div');
+function createContentBody() {
+  const contentBody = document.createElement("div");
   contentBody.setAttribute("id", "content");
 
   return contentBody;
 }
 
-function createFooter(){
-  const footer = document.createElement('footer');
-  const message = document.createElement('p');
+function createFooter() {
+  const footer = document.createElement("footer");
+  const message = document.createElement("p");
   message.textContent = "Connect with us:";
 
-  const ul = document.createElement('ul');
-  
+  const ul = document.createElement("ul");
+
   const icons = [fbIcon, igIcon, tiktokIcon];
-  icons.forEach(icon => {
-    const li = document.createElement('li');
-    const href = document.createElement('a');
-    const img = document.createElement('img');
+  icons.forEach((icon) => {
+    const li = document.createElement("li");
+    const href = document.createElement("a");
+    const img = document.createElement("img");
     img.src = icon;
 
     href.appendChild(img);
     li.appendChild(href);
     ul.appendChild(li);
-
   });
 
   footer.appendChild(message);
@@ -65,8 +64,8 @@ function createFooter(){
   return footer;
 }
 
-export default function createLandingPage(){
-  document.body.appendChild(createHeader());
-  document.body.appendChild(createContentBody());
-  document.body.appendChild(createFooter());
+export default function createLandingPage(body) {
+  body.appendChild(createHeader());
+  body.appendChild(createContentBody());
+  body.appendChild(createFooter());
 }
