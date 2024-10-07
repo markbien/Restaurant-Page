ce6efc164d3d23285d1f850fe50bf14530ccdeb4
Mark Bien <48498353+markbien@users.noreply.github.com>
2024-10-07 13:47:25 +0800

    Call createLandingPage and pass document.body

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
-console.log("Hello, world!");
-console.log("This is a test!");
+import './style.css';
+import createLandingPage from './modules/landing-page.js';
+
+createLandingPage(document.body);
